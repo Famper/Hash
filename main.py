74a42c76711496a172226900eb47692ae22e65e7
dr_fap <37632821+Famper@users.noreply.github.com>
2024-03-21 21:13:49 +0000

    fix

--- main.py
+++ main.py
@@ -5,23 +5,10 @@ if __name__ == '__main__':
 
     print(f"\nHash object: {hash_object.get_hash_set()}")
 
-    hash_object.append('1')
-    hash_object.append('2')
-    hash_object.append('3')
-    hash_object.append('4')
-    hash_object.append('5')
-    hash_object.append('6')
-    hash_object.append('7')
-    hash_object.append('8')
-    hash_object.append('9')
-    hash_object.append('10')
-    hash_object.append('11')
+    with open('lorem_ipsum.txt') as f:
+        contents = f.read()
 
-    print(f"\nHash object: {hash_object.get_hash_set()}")
-
-    hash_object.remove('1')
-    hash_object.remove('0')
-    hash_object.remove('2')
-    hash_object.remove('10')
+        for word in contents.split():
+            hash_object.append(word)
 
     print(f"\nHash object: {hash_object.get_hash_set()}")
